e0ebd2e170da48f94d7883804454d08737ed5e62
monopolizante <robsonlaurensan@gmail.com>
2026-02-16 16:35:10 -0300

    Faz estrutura básica e funcional do projeto

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ import axios from 'axios'
 
 const port = 3000
 const app = express()
+app.use(express.urlencoded({ extended: true}))
+
+app.get("/" , (req, res) =>{
+    res.render("index.ejs")
+})
+
+app.post("/pesquisar" , async (req, res) => {
+    try{
+        let palavra = req.body.palavra
+        let response = await axios.get(`https://freedictionaryapi.com/api/v1/entries/en/${palavra}`)
+        let synonyms = response.data.entries[0].synonyms
+        res.render("index.ejs", {sinonimos: synonyms})
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 app.listen(port, () => {
     console.log(`Servidor ligado na porta ${port}`)
