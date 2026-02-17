b85ecea2aeec655f56c79543666074933e9920e6
monopolizante <robsonlaurensan@gmail.com>
2026-02-17 12:30:57 -0300

    Adiciona animações básicas fade-in

---
+++ animation.js
@@ -0,0 +1,1 @@
+let sinonimo = document.querySelectorAll(".sinonimo")
--- index.js
+++ index.js
@@ -3,18 +3,34 @@ import axios from 'axios'
 
 const port = 3000
 const app = express()
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
-app.get("/" , (req, res) =>{
+app.get("/", (req, res) => {
     res.render("index.ejs")
 })
 
-app.post("/pesquisar" , async (req, res) => {
-    try{
+app.post("/pesquisar", async (req, res) => {
+    try {
         let palavra = req.body.palavra
         let response = await axios.get(`https://freedictionaryapi.com/api/v1/entries/en/${palavra}`)
         let synonyms = response.data.entries[0].synonyms
-        res.render("index.ejs", {sinonimos: synonyms})
+        let timing = []
+        let i = 0
+        let seconds = true
+        synonyms.forEach((sinonimo) => {
+            if (i === 10) {
+                seconds = false;
+                i = 0;
+            }
+            if (seconds) {
+                timing.push(`animation-duration:0.${i}s;`);
+            } else {
+                timing.push(`animation-duration:1.${i}s;`);
+            }
+
+            i++;
+        })
+        res.render("index.ejs", { sinonimos: synonyms, duration: timing, j: 0 })
     } catch (error) {
         console.log(error)
     }
